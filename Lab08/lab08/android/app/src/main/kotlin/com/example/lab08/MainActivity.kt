c7c300ba774bae84700b88f92a29cd42642e3495
Gautam7502 <101510617+Gautam70502@users.noreply.github.com>
2022-09-19 21:01:08 +0530

    Add files via upload

---
+++ Lab08/lab08/android/app/src/main/kotlin/com/example/lab08/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab08
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Lab08/lab08/lib/main.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:lab08/quote.dart';
+
+// state full widget
+void main() => runApp(MaterialApp(
+  home: FinalTest1(),
+));
+class FinalTest1 extends StatefulWidget {
+  @override
+  State<FinalTest1> createState() => _FinalTest1State();
+}
+
+class _FinalTest1State extends State<FinalTest1> {
+// const FinalTest1({Key? key}) : super(key: key);
+  num age =20;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[200],
+      appBar: AppBar(
+        title: Text('First App'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+        elevation: 0.0,
+      ),
+    // add button
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            age+=1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.brown,
+      ),
+      // remove button
+    /*  floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            age-=1;
+          });
+        },
+        child: Icon(Icons.remove),
+        backgroundColor: Colors.brown,
+      ),
+    */
+
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/sub_assets/dog1.jpg'),
+            ),
+            Text(
+              'NAME: ',
+              style: TextStyle(
+                color: Colors.grey[800],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'RATHOD GAUTAM',
+              style: TextStyle(
+                color: Colors.blue[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 40),
+            Text(
+              'age',
+              style: TextStyle(
+                color: Colors.grey[800],
+                letterSpacing: 2.0,
+
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+
+              '$age',
+              style: TextStyle(
+                color: Colors.blue[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 50),
+            Row(
+              children: [
+                Icon(
+                  Icons.email_rounded,
+                  color: Colors.deepPurple[800],
+                ),
+                SizedBox(width: 12.0),
+                Text(
+                  'gautamrathod70502@gmail.com',
+
+                  style: TextStyle(
+
+                    color: Colors.brown[800],
+                    fontSize: 16.0,
+                    letterSpacing: 1.5,
+
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Quotes
+/*void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+/*
+List<String> quotes = [
+'The truth is realy pure and never simple',
+'I see humans but no humanity',
+'The time is always right to do what is right'
+];
+// List<String> author = [];
+*/
+/* this will create error..because list is now not of string....
+List<String> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'jignesh1'),
+];
+*/
+  List<Quote> quotes = [
+    Quote(text: 'The truth is realy pure and never simple',author:
+    'Gautam'),
+    Quote(author: 'Gautam1', text: 'I see humans but no humanity'),
+    Quote(text: 'The time is always right to do what is right',author:
+    'Gautam2'),
+  ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 20,
+
+                color: Colors.deepPurple,
+
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+
+                color: Colors.deepPurple,
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent[100],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}
+*/
